35407f8a23e0b23cf8e52037cb52c7360b4f0990
imkemp <ikemp@kingcounty.gov>
2023-10-05 16:21:50 -0700

    Task set-up

---
+++ data visualization goals.R
@@ -0,0 +1,11 @@
+## GOALS: 
+
+## Read in data (Hydrology_UKEZC, in "data" folder of this project)
+
+
+## Visualize daily discharge over time from 2001-2003, coloring the values which are "E" (estimated) vs those with no flags
+
+
+## Visualize water temperature for 2022-2023 on a shared x-axis spanning the first to last day of the year
+
+
