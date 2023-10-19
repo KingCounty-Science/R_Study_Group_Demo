c9e2ee13b8b98ea72051b778b914449b4ec19594
greg-ikeda <gregikeda@gmail.com>
2023-10-19 16:36:04 -0700

    Erased EVERYTHING!!!
    Starting from a clean slate, so everyone can test working from the main branch.

--- data visualization goals.R
+++ data visualization goals.R
@@ -7,7 +7,6 @@
 
 ## Visualize water temperature for 2022-2023 on a shared x-axis spanning the first to last day of the year
 
-#Bailey CREATING CONFLICT 
 # Setup -------------------------------------------------------------------
 
 library(tidyverse)
@@ -18,51 +17,16 @@ library(plotly)
 
 # Read in data ------------------------------------------------------------
 
-# Read csv file into R and change the two datetime columns into proper date/time formats
 
-# Adding notgreg_ plus some additional code to the main branch of "data visualization goals.R" so we can see how new code + code changes work. 
-
-notgreg_hydro_data <- read_csv(here("Hydrology_UKEZC_bk.csv")) %>%
-  mutate(CollectDate_UTC = mdy_hm(CollectDate_UTC),
-        `Collect Date_PDT` = mdy_hm(`Collect Date_PDT`),
-        date = date(`Collect Date_PDT`))
-
-# Checking to make sure that the date range is as expected (2001 - 2023) 
-summary(notgreg_hydro_data$CollectDate_UTC)
-
-# Creating seperate dataframe for 2001 - 2003 data
-notgreg_hydro_01_03 <- hydro_data %>%
-  filter(date >= mdy("1-1-2001"),
-         date < mdy("1-1-2004"))
-
-summary(notgreg_hydro_01_03$date)
 
 
 # Visualize daily discharge over 2001 - 2003 -------------------------------------
 
-hydro_daily_discharge <- hydro_01_03 %>%
-  group_by(date) %>%
-  summarize(daily_discharge_avg = mean(Discharge_cfs))
-
-# Append daily discharge values to the full dataframe to retain the error flags. 
-# Note that error flags apply to 15-minute samples, not the daily averages. Will need to think through that!
-hydro_withaverages <- left_join(hydro_01_03, hydro_daily_discharge,
-                  by = join_by(date))
-
-# Plotting all 2001 - 2003 data, coloring points by the Flag Value
-# Not separating by year yet
-testplot <- ggplot(hydro_withaverages)+
-  geom_point(aes(x = CollectDate_UTC,
-                 y = daily_discharge_avg,
-                 color = as.factor(Flag_StageQ)))
 
 
 
+# Visualize water temperature for 2022-2023 on a shared x-axis spa --------
 
-# Testing conflicts -------------------------------------------------------
 
-1+1
-2+2
 
-cool_person = "Nicholas Cage"
 
