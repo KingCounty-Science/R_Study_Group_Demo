f6a32facc092b0821377af6fb8ab6125bdd30288
Wafa Tafesh <126634324+wtafesh@users.noreply.github.com>
2023-11-02 12:27:16 -0700

    read in data, changed headers and tried a vis plot, so much data!

--- data visualization goals.R
+++ data visualization goals.R
@@ -7,22 +7,38 @@
 
 ## Visualize water temperature for 2022-2023 on a shared x-axis spanning the first to last day of the year
 
+rm (list=ls())
+
 # Setup -------------------------------------------------------------------
 
 library(tidyverse)
 library(lubridate)
 library(here())
 library(plotly)
+library(anytime)
 
 
 # Read in data ------------------------------------------------------------
+# read in data and ignore column header reading errors (reads in as is)
+hydro <- read.csv("C:/GitHub/R_Study_Group_Demo/data/Hydrology_UKEZC.csv", check.names = F)
 
-
+# change headers to no spaces
+colnames(hydro)[2] <- "date_UTC"
+colnames(hydro)[3] <- "date_local"
+colnames(hydro)[4] <- "stage_ft"
+colnames(hydro)[5] <- "discharge_cfs"
+colnames(hydro)[6] <- "discharge_qualifier"
+colnames(hydro)[7] <- "turbidity_NTU"
+colnames(hydro)[8] <- "turbidity_qualifier"
+colnames(hydro)[9] <- "temperature_C"
+colnames(hydro)[10] <- "temperature_qualifier"
 
 
 # Visualize daily discharge over 2001 - 2003 -------------------------------------
 
-
+ggplot(subset(hydro, discharge_qualifier == "P")) +
+  geom_point(aes(x=date_local, y=discharge_cfs, color=discharge_qualifier)) +
+  theme_bw()
 
 
 # Visualize water temperature for 2022-2023 on a shared x-axis spa --------
