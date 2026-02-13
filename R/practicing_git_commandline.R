3442072ca3a4aaa556646526c26f9d9a09c93ac9
freshbeka <rstiling@kingcounty.gov>
2026-02-13 09:28:17 -0800

    Added a few lines to script in R folder

---
+++ R/practicing_git_commandline.R
@@ -0,0 +1,4 @@
+## This is a test script. I want to to test using the git checkout call incase I save something that I don't mean to.
+
+
+#on line 4, push to the repo. On line 6, write somthing, then save it, but don't push. Try checkout to get back the pre-saved version.
